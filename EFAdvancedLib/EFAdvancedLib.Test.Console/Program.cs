d1984aa1a4b8075c079e44ea41128e95795b6f3c
Oğuz KURTCUOĞLU <oguzkurtcuoglu@outlook.com>
2015-08-23 01:09:09 +0300

    First

---
+++ EFAdvancedLib/EFAdvancedLib.Test.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EFAdvancedLib.Extensions;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using EntityFramework.BulkInsert.Extensions;
+
+namespace EFAdvancedLib.Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new TestDbEntities())
+            {
+                List<EnrouteDatalari> list = new List<EnrouteDatalari>();
+                Random rnd = new Random();
+                for (int i = 0; i < 750000; i++)
+                {
+                    list.Add(new EnrouteDatalari
+                    {
+                        Birthday = DateTime.Now,
+                        Hit = i * 2,
+                        IsDraft = Convert.ToBoolean(rnd.Next(0, 1)),
+                        Name = string.Concat("Name - ", i),
+                        Surname = string.Concat("Surname - ", i)
+                    });
+
+                }
+
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                System.Console.WriteLine("Started : {0}", DateTime.Now.ToLongTimeString());
+                db.EnrouteDatalari.BulkInsert(list, db, new BulkInsertOptions() { });
+                sw.Stop();
+                System.Console.WriteLine("Stopped : {0} - Elapset : {1}", DateTime.Now.ToLongTimeString(), sw.ElapsedMilliseconds);
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ EFAdvancedLib/EFAdvancedLib/Extensions/DbSetExtensions.cs
@@ -0,0 +1,37 @@
+﻿using EntityFramework.BulkInsert.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace EFAdvancedLib.Extensions
+{
+    public static class DbSetExtensions
+    {
+        public static void BulkInsert<TEntity>(this DbSet<TEntity> dbSet, IEnumerable<TEntity> entities, DbContext context, IDbTransaction transaction, SqlBulkCopyOptions sqlBulkCopyOptions = SqlBulkCopyOptions.Default, int? batchSize = null) where TEntity : class
+        {
+            if (entities == null)
+            {
+                throw new ArgumentNullException(nameof(entities));
+            }
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            context.BulkInsert(entities, transaction, sqlBulkCopyOptions, batchSize);
+        }
+        public static void BulkInsert<TEntity>(this DbSet<TEntity> dbSet, IEnumerable<TEntity> entities, DbContext context, BulkInsertOptions options) where TEntity : class
+        {
+            if (entities == null)
+            {
+                throw new ArgumentNullException(nameof(entities));
+            }
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            context.BulkInsert(entities, options);
+        }
+    }
+}
